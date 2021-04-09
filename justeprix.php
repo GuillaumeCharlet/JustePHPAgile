b47e799ad7327a63ae0990ed2938c25f217e409e
GuillaumeCharlet <33938464+GuillaumeCharlet@users.noreply.github.com>
2021-04-09 14:30:52 +0200

    Add files via upload

---
+++ justeprix.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+    <body>
+        <?php
+        $coup = 0;
+        $file = fopen("historique_chiffres.txt", "w");
+        $min = 0;
+        $max = 100;
+        $nb = rand($min, $max);
+        fwrite($file, $nb);
+        fclose($file);
+
+        $fileh = fopen("historique.txt", "w");
+        fwrite($fileh,"");
+        fclose($fileh);
+        ?>
+        <h1 class="titre">Le Juste Prix !</h1>
+            <form action="testvaleur.php" method="post">
+                <article class="espace_rep">
+                    <p><input type="text" class="champ_rep" name="reponse" placeholder="Ta réponse !">
+                    <input type="hidden" name="coup" value="0">
+                    <button class="validate" type="submit" value="Plus" name="plus">   Valider !  </button></p>
+                </article>
+                <p class="compteur"> Coups: <?php echo ("$coup"); ?> </p>
+            </form>
+        
+    </body>
+</html>
---
+++ testvaleur.php
@@ -0,0 +1,81 @@
+<html>
+<meta charset = "utf-8">
+    <?php
+        $go = "hidden";
+        $win = "hidden";
+        $coup = $_POST["coup"] +1;
+        $rep = $_POST["reponse"];
+        $file = fopen("historique_chiffres.txt", "r") or die ("Impossible d'ouvrir le fichier !");
+        $chiffre = fread($file,filesize("historique_chiffres.txt"));
+        // echo (" Le chiffre mystère: $chiffre <br>");
+        fclose ($file);
+
+        $fileh = fopen("historique.txt", "a");
+        $nbhis = $rep;
+        fwrite($fileh,$nbhis."\n");
+        fclose($fileh);
+    ?>
+    <h1 class="titre">Le Juste Prix !</h1>
+        <form action="testvaleur.php" method="post">
+            <article class="espace_rep">
+                <p><input type="text" class="champ_rep" name="reponse" placeholder="Ta réponse !"></p>
+                <input type="hidden" name="coup" value="<?php echo $coup; ?>">
+                <button class="validate" type="submit" value="Plus" name="plus">   Valider !  </button>
+            </article>
+        <p class="champ_info"> 
+            <?php 
+                echo ("Ta réponse: $rep <br>");
+            ?> 
+        </p>
+        <p class="champ_indiq">
+            <?php
+            if ($coup < 1000) {
+                if ($rep < $chiffre && $rep < 0) {
+                    echo ("Tu dois entrer une valeur entre 0 et 100 seulement !");
+                }
+                else if ($rep < $chiffre) {
+                    echo ("C'est plus !");
+                }
+                else if ($rep > $chiffre && $rep > 100) {
+                    echo ("Tu dois entrer une valeur entre 0 et 100 seulement !");
+                }
+                else if ($rep > $chiffre) {
+                    echo ("C'est moins !");
+                }
+                else if ($rep == $chiffre) { 
+                    $go = "hidden";
+                    $win = "visible";
+                } 
+            } else if ($coup == 10) {
+                $win = "hidden";
+                $go = "visible";
+            }
+                ?>
+        </p>
+        <p class="compteur">
+            Coups:
+                <?php
+                    echo $coup ;
+                ?>
+        </p>
+
+        <p class="historique">
+            Votre historique:  <br> 
+            <?php
+                $fileh = fopen("historique.txt", "r");
+                $fileh2 =  fread ($fileh, 255);
+                fclose ($fileh);
+                echo nl2br ($fileh2); 
+            ?>
+        </p>
+
+        <div class="gagner" style="visibility: <?php echo $win ?>">
+            <p class="paragag">Bravo tu as gagner !</p>
+            <a class="reco" href="justeprix.php">Recommencer une partie !</a>
+        </div>
+        <div class="perdu"  style="visibility: <?php echo $go ?>">
+            <p class="paraper">Dommage tu as perdu !</p>
+            <a class="reco" href="justeprix.php">Recommencer une partie !</a>
+        </div>
+    </form>
+</html>
